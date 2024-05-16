fe9e139f04e49c7b6c881e3acf5c4b30e9511339
Nicole Venner <nvenner@protonmail.ch>
2024-05-15 19:52:01 -0600

    more stuff

--- Bubbles/Naturals.lean
+++ Bubbles/Naturals.lean
@@ -5,6 +5,13 @@ open Nat
 theorem zeroneq (n : Nat) : n+1 ≠ 0 := by 
 simp [Nat.add_one_ne_zero]
 
+
+theorem addneq (n m : Nat) : n + m +1 ≠ m := by 
+induction m with
+| zero => simp
+| succ m ih => simp; exact ih 
+
+
 theorem succ_inj (n m : Nat) : n=m ↔ n+1 = m+1 := by 
 constructor
 intro h 
@@ -27,7 +34,8 @@ simp
 
 
 
--- xdrcfvgbhnjmkjnhbgvfcdxsecrfvgtbyhnujmikjnhbgvfcdxsedcfvgbnhjbvgyftcdxszdecfvgbnhjubgvfcdxszaxdcfvgbhnbgvfcdxszasxdcfvgbhnbgvfcdxs
+
+
 theorem eq_decidable (n m : Nat) : n=m ∨ ¬ (n=m) := by
   have zeroeq_decidable (n : Nat) :n=0 ∨ ¬ (n=0) := by
     induction' n with n nh
@@ -41,28 +49,19 @@ theorem eq_decidable (n m : Nat) : n=m ∨ ¬ (n=m) := by
     exact Or.inl hleft.symm
     have fhright := fun x : 0=n => hright x.symm 
     exact Or.inr fhright
-  induction' n with n nh
-  exact zeroeq_decidable_flipped m
-  induction' m with m mh 
-  exact Or.inr (zeroneq n)
-  sorry 
-
-
-theorem eq_decidable_snd (n m : Nat) : n=m ∨ ¬ (n=m) := by
-  have zeroeq_decidable (n : Nat) :n=0 ∨ ¬ (n=0) := by
-    induction' n with n nh
-    have zeroeq : 0=0 := by rfl;
-    exact Or.inl zeroeq
-    have nplusneqzero : n+1 ≠ 0 := zeroneq n 
-    exact Or.inr nplusneqzero
-  have zeroeq_decidable_flipped (n : Nat) : 0 = n ∨ ¬0 = n := by 
-    have nzero := zeroeq_decidable n
-    cases' nzero with hleft hright 
-    exact Or.inl hleft.symm
-    have fhright := fun x : 0=n => hright x.symm 
-    exact Or.inr fhright
-  induction n m with 
-  | 0 , 0 => by rfl;
+  induction n with 
+  | zero  => exact zeroeq_decidable_flipped m 
+  | succ k ih => 
+    cases ih with
+    | inl lh => 
+      have khm : k+1 ≠ m := by 
+        simp [lh]
+      exact Or.inr khm
+    | inr rh => 
+      induction m with 
+      | zero => exact Or.inr (zeroneq k)
+      | succ m ih => sorry
+    
 
   
 
